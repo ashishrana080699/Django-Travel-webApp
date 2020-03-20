58109abda3e0f3be5c2a4203bb069595cd144d2a
Ashish Rana <aashishrana269@gmail.com>
2020-03-21 04:41:52 +0530

    Delete urls.py

--- Travello/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.Home, name='home'),
-    path('index.html', views.Home, name='home'),
-    path('about.html',views.About,name='about'),
-    path('news.html', views.News, name='news'),
-]
